8fd6447802894d67a112c261337c49b6f43ae528
K_A_A <banderas91@mail.ru>
2022-08-23 15:23:02 +0300

    семинар_2_задача_1

---
+++ Seminar2_zadacha001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход случайно сгенерированное трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int start = 100;
+int stop = 999;
+
+int value = new Random().Next(start, stop + 1);
+Console.WriteLine(value);
+
+int digit_second = (value % 100) / 10;
+
+Console.WriteLine(digit_second);
