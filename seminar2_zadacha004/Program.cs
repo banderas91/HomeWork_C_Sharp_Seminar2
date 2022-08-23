4d5a0c23845984d85c6c3f146b30f6d570e98c14
K_A_A <banderas91@mail.ru>
2022-08-23 16:06:57 +0300

    семинар_2_задача_4

---
+++ seminar2_zadacha004/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4:
+// Напишите программу, которая принимает на вход случайно сгенерированное число (от 1 до 1 000 000) и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+
+int start = 1;
+int stop = 1000000;
+
+int value = new Random().Next(start, stop + 1);
+Console.WriteLine(value);
+
+
+if (value % 7 == 0 && value % 23 == 0) 
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    
+   Console.WriteLine("НЕ кратно");
+}
+
